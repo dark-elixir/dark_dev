9016f1d1d7b50f08553f3a01381c89f1a09856b3
Michael Sitchenko <michael.sitchenko@gmail.com>
2020-08-30 19:49:58 -0400

    bump v1.0.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DarkDev.MixProject do
 
   use Mix.Project
 
-  @version "1.0.2"
+  @version "1.0.3"
   @name "DarkDev"
   @hexpm_url "http://hexdocs.pm/dark_dev"
   @github_url "https://github.com/dark-elixir/dark_dev"
